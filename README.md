56448c728a5a84f0ff9f46ca6a4ee6bb7fa5102c
p <ppppppppppppppppp@pm.me>
2021-10-08 17:07:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sienna Vest
+
+Manually call vest on RPT if needed.
+
+## Getting started
+
+`$ yarn setup`
+
+`$ yarn dev`
+
+## Testing if the build compiles
+
+Run `$ yarn build`
---
+++ api/vesting.ts
@@ -0,0 +1,24 @@
+import { StdFee } from 'secretjs/types/types';
+import { SigningCosmWasmClient } from 'secretjs';
+
+// Claims vested tokens. Useful for investors.
+export const claimVestedTokens = (secretjs: SigningCosmWasmClient, address: string, fee?: StdFee) => {
+  return secretjs.execute(address, {
+    claim: {},
+  },
+    '',
+    [],
+    fee
+  );
+};
+
+
+export const callVestOnRPT = (secretjs: SigningCosmWasmClient, address: string, fee?: StdFee) => {
+  return secretjs.execute(address, {
+    vest: {},
+  },
+    '',
+    [],
+    fee
+  );
+};
